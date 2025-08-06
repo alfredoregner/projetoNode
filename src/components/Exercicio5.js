40ed289680c62d85bb9534a39f338234743e35ee
Alfredo Regner <94856772+alfredoregner@users.noreply.github.com>
2025-08-06 14:29:58 -0300

    Exercício 5
    Finalizado a programação do exercício 5

--- src/components/Exercicio5.js
+++ src/components/Exercicio5.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+// function ListaFrutas(frutas) {
+//   return (
+//     <div>
+//       <li>{frutas}</li>
+//       <button>Adicionar Fruta</button>
+//     </div>
+//   );
+// }
+
 
 function ListaFrutas() {
+  const [frutas, setFrutas] = useState(['Maçã', 'Banana', 'Uva']);
+
+  const novasFrutas = ['Morango', 'Laranja', 'Abacaxi', 'Pera', 'Manga', 'Kiwi', 'Melancia']
+
+  const adicionarFruta = () => {
+    const frutaAleatoria = novasFrutas[Math.floor(Math.random() * novasFrutas.length)];
+    setFrutas([...frutas, frutaAleatoria]);
+  };
+
   return (
     <div>
-      <button>Adicionar Fruta</button>
+      <ul>
+        {frutas.map((fruta, index) => (
+          <li key={index}>{fruta}</li>
+        ))}
+      </ul>
+      <button onClick={adicionarFruta}>Adicionar Fruta</button>
     </div>
   );
 }
